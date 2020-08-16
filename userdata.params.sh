9d482c7899cf16f01531982b5b110292dfe7d9ea
Gabriel West <westgabr@amazon.com>
2020-08-16 14:11:48 +0100

    fix paths

--- userdata.params.sh
+++ userdata.params.sh
@@ -1,5 +1,5 @@
 set -euf -o pipefail
 
-ICECAST_CONFIG_XML=$(./template.sh icecast.template.xml icecast.params.sh)
+ICECAST_CONFIG_XML=$(./template.sh ./icecast.template.xml ./icecast.params.sh)
 ICECAST_DEFAULT=$(cat ./icecast2_defaults.sh)
 SILENCE_BASE64=$(base64 ./silence_stereo_192.mp3)
